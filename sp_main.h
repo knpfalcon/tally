a0b43aeb0622f7c6b0f3d6fb130ebca0ae15abe1
knpfalcon <knpfalcon@gmail.com>
2019-04-20 14:22:48 -0400

    Added sp_main.h

---
+++ sp_main.h
@@ -0,0 +1,21 @@
+#ifndef SP_MAIN_H
+#define SP_MAIN_H
+
+#include "jt_util.h"
+
+//Macro hack for tile_sheet coordinates... It works, by God!
+#define TILE001 448, 112
+#define TILE002 400, 112
+#define TILE003 128, 32
+
+#define TILE_SIZE 16
+
+#define MAP_WIDTH 128
+#define MAP_HEIGHT 90
+
+#define DISPLAY_MULTIPLIER 7
+
+#define FONT_FILE "fixed_01.ttf"
+
+
+#endif // SP_MAIN_H
