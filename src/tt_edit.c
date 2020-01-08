cdb8946dc02aa4016c5716ea9bc7fb07151d049b
knpfalcon <knpfalcon@gmail.com>
2020-01-08 03:28:37 -0500

    Updated todo.

--- src/tt_edit.c
+++ src/tt_edit.c
@@ -38,7 +38,6 @@ t_conditional cond = {false, false, false, false};
 
 //const char *filename = NULL;
 
-
 //enum KEYS {KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_Z, KEY_LSHIFT, KEY_N,KEY_PAD_PLUS, KEY_PAD_MINUS};
 bool key[16] = {false};
 enum KEYS {KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_Z, KEY_LSHIFT, KEY_X, KEY_T, KEY_N, KEY_PAD_PLUS, KEY_PAD_MINUS, KEY_E, KEY_R, KEY_Q, KEY_F, KEY_G};
